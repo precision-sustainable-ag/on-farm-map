0608b08f5488012fd998c9c6897f80b307a7692a
Brian W. Davis <brianwdavis@gmail.com>
2022-03-31 11:54:33 -0400

    move components into UI; add states mask; convert to API

--- server.R
+++ server.R
@@ -2,6 +2,7 @@ library(leaflet)
 library(mapview)
 library(purrr)
 library(dplyr)
+library(httr)
 
 
 
@@ -13,9 +14,13 @@ httr::set_config(httr::config(http_version = 0))
 
 source("secret.R")
 
-program_locations <- readr::read_csv("programs.csv")
-#css_awesome_cols <- readr::read_csv("css_awesome_cols.csv")
+program_locations <- readr::read_csv("programs.csv", show_col_types = F)
+mask_world <- read_sf("background_mask.geojson")
 
+abb <- tibble(name = state.name, abb = state.abb)
+states <- sf::read_sf("us-states.geojson") %>% 
+  left_join(abb) %>% 
+  filter(abb != "AK")
 
 server <- function(input, output, session) {
 
@@ -34,48 +39,76 @@ server <- function(input, output, session) {
     )
   })
   
-  output$col <- renderUI({
-    radioButtons(
-      "col", "Marker colors",
-      choices = c(
-        RColorBrewer::brewer.pal.info %>% filter(category == "qual") %>% rownames()
-        ),
-      selected = "Set1"
-    )
-  })  
+  # output$col <- renderUI({
+  #   radioButtons(
+  #     "col", "Marker colors",
+  #     choices = c(
+  #       RColorBrewer::brewer.pal.info %>% 
+  #         filter(category == "qual") %>% 
+  #         rownames()
+  #       ),
+  #     selected = "Set1"
+  #   )
+  # })  
   
   cols <- reactive({
-    scales::brewer_pal(palette = input$col)(4)
+    scales::brewer_pal(palette = input$col)(length(input$exps)) %>% 
+      set_names(input$exps)
   })
   
-  sites <- safely(tbl)(con, "site_information")
+  # sites <- safely(tbl)(con, "site_information")
+  # 
+  # if (is.null(sites$result)) {
+  #   con <- dbConnect(
+  #     Postgres(),
+  #     dbname = pg_dbname,
+  #     host = pg_host,
+  #     port = pg_port,
+  #     user = pg_user,
+  #     password = pg_password,
+  #     sslmode = "require"
+  #   )
+  #   
+  #   sites <- safely(tbl)(con, "site_information")
+  # }
   
-  if (is.null(sites$result)) {
-    con <- dbConnect(
-      Postgres(),
-      dbname = pg_dbname,
-      host = pg_host,
-      port = pg_port,
-      user = pg_user,
-      password = pg_password,
-      sslmode = "require"
+  sites_res <- purrr::safely(RETRY)(
+    "GET",
+    url = "https://api.precisionsustainableag.org/onfarm/locations",
+    query = list(
+      output = "json"
+    ),
+    add_headers(
+      "x-api-key" = api_key
     )
-    
-    sites <- safely(tbl)(con, "site_information")
-  }
+  )
   
-
-  all_sites <- sites$result %>% 
-    select(code, year, affiliation, longitude, latitude, producer_id, protocols_enrolled) %>% 
-    filter(!is.na(latitude)) %>% 
-    filter(is.na(protocols_enrolled) | protocols_enrolled == "-999") %>% 
-    collect() %>% 
-    distinct(code, .keep_all = TRUE) %>% 
+  all_sites <- content(sites_res$result, as = "text") %>% 
+    jsonlite::fromJSON() %>% 
+    filter(affiliation != "DV") %>% 
     filter(
-      between(latitude, 24, 50),
-      between(longitude, -125, -65)
-    )
-  
+      !is.na(lat), !is.na(lon),
+      lat != -999, lon != -999
+      ) %>% 
+    filter(lat > 0, lat < 90, lon < 0, lon > -180) %>% 
+    group_by(code, year, affiliation) %>% 
+    summarise(
+      lat = mean(lat, na.rm = T), 
+      lon = mean(lon, na.rm = T)
+      ) 
+
+  # all_sites <- sites$result %>% 
+  #   filter(affiliation != "DV") %>% 
+  #   select(code, year, affiliation, longitude, latitude, producer_id, protocols_enrolled) %>% 
+  #   filter(!is.na(latitude)) %>% 
+  #   filter(is.na(protocols_enrolled) | protocols_enrolled == "-999") %>% 
+  #   collect() %>% 
+  #   distinct(code, .keep_all = TRUE) %>% 
+  #   filter(
+  #     between(latitude, 24, 55),
+  #     between(longitude, -125, -65)
+  #   )
+  # 
   
   output$affiliations <- renderUI({
     checkboxGroupInput(
@@ -96,35 +129,79 @@ server <- function(input, output, session) {
 
   
   #sites
-  ico <- reactive({
-    awesomeIcons(
-      icon = "unchecked", 
-      #squareMarker = T, 
-      iconColor = css_awesome_cols %>% 
-        filter(color == input$col) %>% 
-        pull(col),
-      markerColor = input$col
-      )
-    })
+  # ico <- reactive({
+  #   awesomeIcons(
+  #     icon = "unchecked", 
+  #     #squareMarker = T, 
+  #     iconColor = css_awesome_cols %>% 
+  #       filter(color == input$col) %>% 
+  #       pull(col),
+  #     markerColor = input$col
+  #     )
+  #   })
+  
+  some_sites <- reactive({
+    all_sites %>% 
+    filter(
+      affiliation %in% input$affiliations, 
+      year %in% input$yrs
+    ) %>% 
+    st_as_sf(
+      coords = c("lon", "lat"), #c("longitude", "latitude"), 
+      remove = F,
+      crs = 4326
+    ) %>% 
+    mutate(program = "onfarm") %>% 
+    filter(program %in% input$exps)
+  })
+  
+  some_programs <- reactive({
+    program_locations %>% 
+    filter(program %in% input$exps) %>% 
+    st_as_sf(
+      coords = c("longitude", "latitude"), 
+      remove = F,
+      crs = 4326
+    )
+  })
+  
   
   mobj <- reactive({
     if (is.null(input$prov)) return(NULL)
+
+    mask_states <- st_filter(
+      states, 
+      st_union(some_sites()),
+      .predicate = function(x, y) !quietly(st_intersects)(x, y)[["result"]]
+    ) %>% 
+      st_filter(
+        st_union(some_programs()),
+        .predicate = function(x, y) !quietly(st_intersects)(x, y)[["result"]]
+      )
     
     leaflet(options = leafletOptions(attributionControl = F)) %>% 
       addProviderTiles(
         input$prov,
-        options = providerTileOptions(opacity = 0.9)
+        options = providerTileOptions(opacity = input$background)
         ) %>% 
+      addPolygons(
+        data = mask_world,
+        color = "#000000",
+        opacity = 0.1,
+        weight = 1,
+        fillOpacity = input$world_mask
+      ) %>% 
+      addPolygons(
+        data = mask_states,
+        color = "#000000",
+        opacity = 0.0,
+        fillOpacity = input$state_mask
+      ) %>% 
       addCircleMarkers(
-        data = all_sites %>% 
-          filter(
-            affiliation %in% input$affiliations, 
-            year %in% input$yrs
-          ),
-        lat = ~latitude, lng = ~longitude,
+        data = some_sites(),
         opacity = 1,
         radius = 7.5,
-        color = cols()[1] #  "#4daf4a"
+        color = unname(cols()["onfarm"])
       ) %>% 
       addCircleMarkers(
         lat = ~latitude + 
@@ -142,19 +219,31 @@ server <- function(input, output, session) {
         radius = 15,
         fill = NA,
         color = ~case_when(
-          program == "Ed" ~ cols()[2],
-          program == "CE1" ~ cols()[3], 
-          program == "CE2" ~ cols()[4]
+          program == "Ed" ~ unname(cols()["Ed"]),
+          program == "CE1" ~ unname(cols()["CE1"]), 
+          program == "CE2" ~ unname(cols()["CE2"])
           ),
-        data = program_locations,
+        data = some_programs(),
         opacity = 1
-      )
+      ) 
     
   })
   
   output$map <- renderLeaflet({
     input$reset
-    mobj() %>% clearBounds()
+    
+    bounds <- 
+      bind_rows(
+        some_sites(), 
+        some_programs()
+        ) %>% 
+      st_bbox()
+    
+    mobj() %>% 
+      fitBounds(
+        bounds[["xmin"]], bounds[["ymin"]], 
+        bounds[["xmax"]], bounds[["ymax"]]
+      )
   })
 
   output$map_r <- renderUI({
--- ui.R
+++ ui.R
@@ -1,13 +1,77 @@
 library(shiny)
 
+tile_choices <- c(
+  "OpenStreetMap", "OpenStreetMap.BlackAndWhite", "OpenStreetMap.France",
+  "OpenStreetMap.HOT", "Stamen.TonerLite", "Stamen.Terrain", 
+  "Esri", "Esri.WorldTopoMap", "CartoDB", "HikeBike"
+)
+
+
+radio_tiles <- radioButtons(
+  "prov", "Base map",
+  choices = tile_choices, 
+  selected = "OpenStreetMap"
+)
+
+slider_background <- sliderInput(
+  "background", 
+  label = NULL, 
+  min = 0, max = 1, 
+  value = 0.9, step = 0.05,
+  ticks = F
+)
+
+slider_world <- sliderInput(
+  "world_mask", 
+  label = NULL, 
+  min = 0, max = 1, 
+  value = 0.15, step = 0.05,
+  ticks = F
+)
+
+slider_state <- sliderInput(
+  "state_mask", 
+  label = NULL, 
+  min = 0, max = 1, 
+  value = 0.2, step = 0.05,
+  ticks = F
+)
+
+checkbox_projects <- checkboxGroupInput(
+  "exps", "Projects",
+  choices = c(
+    "On-farm" = "onfarm",
+    "CE1" = "CE1",
+    "CE2" = "CE2",
+    "Education" = "Ed"
+  ),
+  selected = c("onfarm", "CE1", "CE2", "Ed")
+)
+
+marker_cols <- radioButtons(
+  "col", "Marker colors",
+  choices = c(
+    RColorBrewer::brewer.pal.info %>% 
+      filter(category == "qual") %>% 
+      rownames()
+  ),
+  selected = "Set1"
+)
+
+
 ui <- fluidPage(
   column(
     3, 
     titlePanel("PSA Sitemap Generator"),
-    uiOutput("prov"), br(), 
     fluidRow(
       column(
-        6, uiOutput("col"), br()
+        6, 
+        radio_tiles,
+        slider_background,
+        slider_world,
+        slider_state,
+        marker_cols,
+        checkbox_projects
         ),
       column(
         6, 
